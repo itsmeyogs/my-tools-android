f91ba792c9a09d3930bbcb91dacf473098037060
yogi <yogiihs28@gmail.com>
2025-08-12 04:21:08 +0700

    add function copy to clipboard

--- app/src/main/java/com/yogs/mytools/util/ViewExtensions.kt
+++ app/src/main/java/com/yogs/mytools/util/ViewExtensions.kt
@@ -1,8 +1,20 @@
 package com.yogs.mytools.util
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.widget.Toast
+import com.yogs.mytools.R
 
 fun Context.showToast(message: String){
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.copyToClipboard(label: String, text:String, message: String? = null){
+    val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clipData = ClipData.newPlainText(label, text)
+    clipboardManager.setPrimaryClip(clipData)
+    val finalMessage = message ?: getString(R.string.message_success_copy_to_clipboard)
+    showToast(finalMessage)
+
 }
