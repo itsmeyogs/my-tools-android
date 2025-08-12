c8b7700d56fdd70be269de23e84464c1e8776fb5
yogi <yogiihs28@gmail.com>
2025-08-12 13:58:55 +0700

    show wifi status and wifi ssid

--- app/src/main/java/com/yogs/mytools/ui/router_manager/RouterManagerActivity.kt
+++ app/src/main/java/com/yogs/mytools/ui/router_manager/RouterManagerActivity.kt
@@ -1,14 +1,16 @@
 package com.yogs.mytools.ui.router_manager
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.yogs.mytools.R
 import com.yogs.mytools.databinding.ActivityRouterManagerBinding
 import com.yogs.mytools.util.setUpAppBar
@@ -25,17 +27,53 @@ class RouterManagerActivity : AppCompatActivity() {
         setUpAppBar(binding.toolbar)
 
         checkAndRequestPermissionLocation()
+        getStatusConnection()
     }
 
 
-//    private fun getStatusConnection(){
-//
-//    }
+    private fun getStatusConnection(){
+        val tvWifiStatus = binding.tvWifiStatus
+        val tvWifiSSID = binding.tvWifiSsid
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        var wifiStatus = getString(R.string.wifi_status_not_connected)
+        var wifiSSID = "-"
+        if(activeNetwork != null){
+            Log.d("activeNetwork" , "object : $activeNetwork")
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+            Log.d("networkCapabilities", "object : $networkCapabilities")
+
+            if(networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)){
+                wifiStatus = getString(R.string.wifi_status_connected)
+                wifiSSID = getWifiSSID(wifiManager)
+            }
+        }
+
+        tvWifiStatus.text = getString(R.string.wifi_status, wifiStatus)
+        tvWifiSSID.text = getString(R.string.wifi_ssid, wifiSSID)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun getWifiSSID(wifiManager: WifiManager): String{
+        val wifiInfo = wifiManager.connectionInfo
+
+        Log.d("network", "object: ${wifiManager.dhcpInfo}")
+        Log.d("network2", "object: ${wifiManager.connectionInfo}")
+        Log.d("network3", "object: ${wifiManager.wifiState}")
+        return wifiInfo.ssid.trim{it == '"'}
+
+//        val dhcpInfo = wifiManager.dhcpInfo
+
+    }
 
     private fun checkAndRequestPermissionLocation(){
         val permission = android.Manifest.permission.ACCESS_FINE_LOCATION
         if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(permission), LOCATION_PERMISSION_REQUEST_CODE)
+        }else{
+            getStatusConnection()
         }
     }
 
@@ -48,9 +86,7 @@ class RouterManagerActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
         if(requestCode == LOCATION_PERMISSION_REQUEST_CODE){
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                showToast(getString(R.string.permission_location_granted))
-            }else{
-                showToast(getString(R.string.permission_location_denied))
+                getStatusConnection()
             }
         }
     }
@@ -58,6 +94,8 @@ class RouterManagerActivity : AppCompatActivity() {
     companion object{
         const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
+
+
 
 
 }
